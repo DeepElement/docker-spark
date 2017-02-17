e9492c8d1683aa0f6f933a21b64f2ed77596ff11
Todd Morrison <todd@deepelement.com>
2017-02-17 10:54:24 -0600

    Better combo lookup

--- branch-gen.sh
+++ branch-gen.sh
@@ -1,24 +1,38 @@
-SPARK_VERSIONS=( '2.0.0' '2.0.1' '2.0.2' '2.0.3' '2.0.4' '2.0.5' )
-HADOOP_VERSIONS=( '2.3' '2.4' '2.6' '2.7' )
+run() {
+    SPARK_VERSION=$1
+    HADOOP_VERSION=$2
+    echo $SPARK_VERSION-$HADOOP_VERSION
 
-for SPARK_VERSION in "${SPARK_VERSIONS[@]}"
-do
-   :
-   for HADOOP_VERSION in "${HADOOP_VERSIONS[@]}"
-   do
-      :
-      git fetch
-      git branch $SPARK_VERSION-$HADOOP_VERSION
-      git checkout $SPARK_VERSION-$HADOOP_VERSION
-      git reset --hard origin/master
-      sed -i -e "s/{SPARK_VERSION}/$SPARK_VERSION/" Dockerfile
-      sed -i -e "s/{HADOOP_VERSION}/$HADOOP_VERSION/" Dockerfile
-      git add Dockerfile
-      git commit -m "Dockerfile udpate"
-      git tag -d $SPARK_VERSION-$HADOOP_VERSION
-      git push --set-upstream origin $SPARK_VERSION-$HADOOP_VERSION -f
-      git clean -df
-   done
-done
+    git fetch
+    git branch $SPARK_VERSION-$HADOOP_VERSION
+    git checkout $SPARK_VERSION-$HADOOP_VERSION
+    git reset --hard origin/master
+    sed -i -e "s/{SPARK_VERSION}/$SPARK_VERSION/" Dockerfile
+    sed -i -e "s/{HADOOP_VERSION}/$HADOOP_VERSION/" Dockerfile
+    git add Dockerfile
+    git commit -m "Dockerfile udpate"
+    git tag -d $SPARK_VERSION-$HADOOP_VERSION
+    git push --set-upstream origin $SPARK_VERSION-$HADOOP_VERSION -f
+    git clean -df
 
-git checkout master
+    # reset master
+    git checkout master
+
+}
+
+run '2.0.0' '2.3'
+run '2.0.0' '2.4'
+run '2.0.0' '2.6'
+run '2.0.0' '2.7'
+run '2.0.1' '2.3'
+run '2.0.1' '2.4'
+run '2.0.1' '2.6'
+run '2.0.1' '2.7'
+run '2.0.2' '2.3'
+run '2.0.2' '2.4'
+run '2.0.2' '2.6'
+run '2.0.2' '2.7'
+run '2.1.0' '2.3'
+run '2.1.0' '2.4'
+run '2.1.0' '2.6'
+run '2.1.0' '2.7'
